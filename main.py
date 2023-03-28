43449507d5d3cf24222c09a8e2580766df7f5460
khoirulamars <khoirulamar@gmail.com>
2023-03-28 11:14:30 +0700

    resolving confic perbaikan

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 print("ini didalam dokumen main.py")
 
-<<<<<<< HEAD
 user_do = input("Anda ingin melakukan registrasi (Y/N):")
 if user_do == "Y":
     print("User akan registrasi")
 else:
     print("User akan subscribe")
-=======
+
 user_do = input("Anda ingin melakukan registrasi (Y/N):")
->>>>>>> 6823f34bd3f7d4b4ac6c06997c0806be338936b2
+
